524255645b4a8b729dffd3aaf5342cf972d9ff87
jasirmukhtar <mukhtarjasir@gmail.com>
2025-03-30 14:30:05 +0530

    CORS Enabled

--- src/main/java/com/example/GenInvoiceApp/Controller/InvoiceController.java
+++ src/main/java/com/example/GenInvoiceApp/Controller/InvoiceController.java
@@ -1,4 +1,16 @@
 package com.example.GenInvoiceApp.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/home")
 public class InvoiceController {
+
+    @GetMapping
+    public String HelloWorld(){
+        return "Hello World from BE";
+    }
+
 }
