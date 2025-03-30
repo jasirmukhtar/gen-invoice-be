dc933774ec32bff64286c879db025cbd8e2bf566
jasirmukhtar <mukhtarjasir@gmail.com>
2025-03-30 11:47:42 +0530

    Intial commit

---
+++ src/main/java/com/example/GenInvoiceApp/GenInvoiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.GenInvoiceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenInvoiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenInvoiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GenInvoiceApp/GenInvoiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GenInvoiceApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenInvoiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
